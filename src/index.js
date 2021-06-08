8b99de46e16f596428c72ef7bf78ac3747f4a81f
Nikolaos P Argalias <nikosgalias@gmail.com>
2021-06-08 10:52:06 +0100

    changed bootstrap dependancies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const $ = require("jquery");
-import "bootstrap";
+import { Tooltip, Toast, Popover } from "bootstrap";
 import "./scss/main.scss";
 import "./js/media-query";
 import "./js/hide-nav";
